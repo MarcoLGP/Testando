a37b3dfee92af828e843607d70bef596fb5cfc6e
MarcoLGP <marcolucagp@gmail.com>
2023-03-20 09:04:38 -0300

    update

--- Testando/Form1.Designer.cs
+++ Testando/Form1.Designer.cs
@@ -39,7 +39,7 @@
             label1.Name = "label1";
             label1.Size = new Size(92, 30);
             label1.TabIndex = 0;
-            label1.Text = "Versão 3";
+            label1.Text = "Versão 4";
             // 
             // Form1
             // 
